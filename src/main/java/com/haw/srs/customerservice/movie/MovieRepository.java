59eec1257068edb2d9c639fce251cdcb5f819b9c
Halil <ultraslanhh@gmail.com>
2023-04-11 03:01:44 +0200

    last try

--- src/main/java/com/haw/srs/customerservice/movie/MovieRepository.java
+++ src/main/java/com/haw/srs/customerservice/movie/MovieRepository.java
@@ -1,10 +1,14 @@
 package com.haw.srs.customerservice.movie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 @Repository //
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-    public Movie findByTitle(String titel);
+
+    @Query("SELECT m FROM Movie m WHERE m.titel = :titel")
+     Movie findByTitle(String titel);
 
 }
--- src/main/java/com/haw/srs/customerservice/reservation/ReservationRepository.java
+++ src/main/java/com/haw/srs/customerservice/reservation/ReservationRepository.java
@@ -3,6 +3,27 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.haw.srs.customerservice.movie.Movie;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long>{
-    public List<Reservation> findAllByMovie(Movie movie);
-}
+    //@Query ("SELECT m FROM Movie m WHERE m.titel =  Movie.titel")
+
+
+ //   @PersistenceContext
+   // private EntityManager entityManager;
+
+
+   // @Transactional
+     public List<Reservation> findAllByMovie(Movie movie);
+   //      return entityManager.createQuery(
+   //              "SELECT r FROM Reservation r WHERE r.movie = movie", Reservation.class).getResultList();
+
+     }
+
+
+
